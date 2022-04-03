2362628a13e7b161c4862db3f79a58dde2ee3074
TochukwuEze <84936116+TochukwuEze@users.noreply.github.com>
2022-04-03 17:20:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import react from 'react';
+import './App.css';
+import CollectionCard from './components/CollectionCard';
+import Header from './components/Header';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import Punklist from './components/Punklist';
+import Main from './components/Main';
+
+
+function App() {
+  const [punkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/assets?asset_contract_address=0x74A0011Fd3CDA397510FCB5Fc2d736Cb793918C7&order_direction=asc'
+        )
+        console.log(openseaData.data.assets)
+        setPunkListData(openseaData.data.assets)
+
+    }
+
+    return getMyNfts()
+  },[])
+  return ( 
+  <div className= 'app'>
+    <Header/>
+    {punkListData.length > 0 && (
+      <>
+        <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
+        <Punklist 
+        punkListData = {punkListData} 
+        setSelectedPunk = {setSelectedPunk} />
+      </>
+    )}
+  </div>
+
+  )
+}
+
+export default App;
